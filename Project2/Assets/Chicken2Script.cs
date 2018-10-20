b278f7e273a54232f12992243e475e47d1abb21f
nadia12344 <npranabudi@gmail.com>
2018-10-20 11:48:20 +1100

    CHICKEN

--- Project2/Assets/Chicken2Script.cs
+++ Project2/Assets/Chicken2Script.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chicken2Script : MonoBehaviour {
-	//This chickenScript allows chicken to move towards players
+	//This chickenScript allows chicken to randomly move within borders of the platform
+	private float speed = 0.05f; 
+	private float maxRotation = 5.0f;
+
 	private GameObject platform; 
 	private GameObject player;
 
@@ -16,6 +19,7 @@ public class Chicken2Script : MonoBehaviour {
 	void Start () {
 		player = Camera.main.gameObject ; 
 		platform = this.transform.parent.gameObject;
+		Vector3 chickenBounds = this.gameObject.GetComponent<Collider> ().bounds.size;
 		Vector3 platfromSize = platform.GetComponent<Collider> ().bounds.size;
 
 		float zSize = platfromSize.z;
@@ -26,6 +30,7 @@ public class Chicken2Script : MonoBehaviour {
 
 		//Randomly positions the chickens
 		Vector3 pos = this.transform.position; 
+		pos.y += chickenBounds.y / 8.0f;
 		pos.z += Random.Range (-zSize,zSize);
 		pos.x += Random.Range (-xSize,xSize);
 		this.transform.position = pos; 
@@ -33,27 +38,113 @@ public class Chicken2Script : MonoBehaviour {
 		//set bounds
 		Vector3 platformPosition = platform.transform.position; 
 		Vector3 chickenSize = this.gameObject.GetComponent<Collider> ().bounds.size;
-		xUpperBound = platformPosition.x + xSize - chickenSize.x; 
-		xLowerBound = platformPosition.x - xSize + chickenSize.x; 
-		zUpperBound = platformPosition.z + zSize - chickenSize.z; 
-		zLowerBound = platformPosition.z - zSize + chickenSize.z; 
+		xUpperBound = platformPosition.x + xSize - chickenSize.x/2; 
+		xLowerBound = platformPosition.x - xSize + chickenSize.x/2; 
+		zUpperBound = platformPosition.z + zSize - chickenSize.z/2; 
+		zLowerBound = platformPosition.z - zSize + chickenSize.z/2; 
 
+		transform.eulerAngles -= new Vector3(0.0f, Random.Range(90.0f,270.0f), 0);
+
+		InvokeRepeating("RotateChicken", 1f, 3.0f);
+		pos = this.transform.position; 
+		if (pos.x <= xUpperBound && pos.x >= xLowerBound && pos.z <= zUpperBound && pos.z >= zLowerBound) {
+			print ("started within bounds");
+			//move towards its forward
+		} else {
+			if (pos.x < xUpperBound || pos.x > xLowerBound) {
+				print ("started outside x bounds");
+				Vector3 newPos = this.transform.position;
+				pos.x = Random.Range (xLowerBound,xUpperBound);
+				this.transform.position = newPos; 
+			}
+			if (pos.z < zUpperBound || pos.z > zLowerBound) {
+				print ("started outside z bounds");
+			}
+
+			//set random x value
+			//set random y value
+			//set random z value
+
+		}
+
+//		xUpperBound -= chickenSize.x/2; 
+//		xLowerBound += chickenSize.x/2; 
+//		zUpperBound -= chickenSize.z/2; 
+//		zLowerBound += chickenSize.z/2; 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		Vector3 center = platform.transform.position; 
+//		//make chicken move towards the vector3 forwards
 		Vector3 curPos = this.transform.position;
-		center.y = curPos.y; 
+//
+//		//if it is within boundaries, move it
+		if (curPos.x <= xUpperBound && curPos.x >= xLowerBound && curPos.z <= zUpperBound && curPos.z >= zLowerBound) {
+			print ("within bounds");
+			//move towards its forward
+			transform.position += this.gameObject.transform.forward.normalized * speed; 
+		} 
+//		//if it is outside boundaries, rotate it so that it can move next time
+		else {
+			if (curPos.x > xUpperBound)
+				print ("outside x's upper bound"); 
+			if (curPos.x < xLowerBound)
+				print ("outside x's upper bound"); 
+			if (curPos.z > zUpperBound)
+				print ("outside z's upper bound"); 
+			if (curPos.z < zLowerBound)
+				print ("outside z's upper bound"); 
+			//rotate the chicken so it can move
+			print ("outside bounds");
 
+			int angle = findMoveableAngle (curPos);
+			if (angle != 0) {
+				transform.eulerAngles += new Vector3 (0.0f, angle, 0);
+				transform.position -= this.gameObject.transform.forward.normalized * 0.5f; 
 
-		//Make chicken move towards player only if it is still within bounds
-		if (curPos.x<=xUpperBound && curPos.x>=xLowerBound && curPos.z<=zUpperBound && curPos.z>=zUpperBound){
-			this.transform.position = Vector3.Lerp(curPos, player.transform.position,Random.Range(0.0f,0.1f));
+			} else {
+				print ("cannot move");
+			}
 		}
-		transform.LookAt (player.transform);
 
+//		print (this.transform.eulerAngles);
 
+	}
+
+	int findMoveableAngle (Vector3 curPos){
+		Vector3 curAngle = transform.eulerAngles;
+//		print ("finding moveable angle");
+		for (int i =90;i<=270; i++){
+			int randomAngle = (int)Random.Range (105.0f, 255.0f);
+			transform.eulerAngles += new Vector3(0.0f, randomAngle, 0.0f);
+			Vector3 newPos = curPos;
+			if (insideBounds (newPos -= this.gameObject.transform.forward.normalized * 0.5f)) {
+				transform.eulerAngles = curAngle; 
+				return randomAngle; 
+			} else {
+//				print ("cannot move at angle " + i);
+				transform.eulerAngles = curAngle; 
+			}
+		}
+
+		transform.eulerAngles = curAngle; 
+		return 0; 
 		
 	}
+	bool insideBounds(Vector3 curPos){
+		if (curPos.x <= xUpperBound && curPos.x >= xLowerBound && curPos.z <= zUpperBound && curPos.z >= zLowerBound) {
+			return true;
+		} 
+		return false; 
+	}
+
+	void RotateChicken(){
+		//rotate the y axis of the chicken
+		float yRotation = Random.Range(-maxRotation,maxRotation);
+		transform.eulerAngles += new Vector3(0.0f, yRotation, 0);
+	}
+
+
+
+
 }
--- Project2/Assets/Project/Scripts/ChickenScript.cs
+++ Project2/Assets/Project/Scripts/ChickenScript.cs
@@ -3,139 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChickenScript : MonoBehaviour {
-	//platform for chicken to move around
-	private Vector3 bounds; 
 	private GameObject platform; 
-//	public GameObject egg; 
-	private float ySize ; 
-	private float speed = 10.0f; 
-	private float dir; 
-	float leftMaxBound; 
-	float downMaxBound;
-	float rightMaxBound;
-	float upMaxBound;
-	float offsetBound;
+	private float thresholdMax ; 
+	private float thresholdMin ; 
+	public GameObject egg; 
 
+	private int dir;
+	private float speed;
 
 	// Use this for initialization
 	void Start () {
+		Vector3 chickenBounds = this.gameObject.GetComponent<Collider> ().bounds.size;
+
+
 		platform = this.transform.parent.gameObject;
-		Vector3 pos = this.transform.position;
-		// print ("pos is originally "+ pos);
-		// print ("bounds is "+bounds); 
-//		print ("pos y is originally "+pos.y);
+		Vector3 platformSize = platform.GetComponent<Collider> ().bounds.size; 
+		float xSize = platformSize.x;
+		float zSize = platformSize.z;
 
-		float zSize = platform.GetComponent<Collider>().bounds.size.z;
-		zSize /= 2; 
+		//Randomly locates the dragon
+		Vector3 position = platform.transform.position;
+		position.y += chickenBounds.y / 8.0f;
+		position.x += Random.Range(-xSize/2,xSize/2); 
+		this.transform.position = position; 
 
-		bounds = platform.GetComponent<Collider>().bounds.size; 
-		//print ("bounds size is "+bounds);
-		leftMaxBound = pos.z - bounds.z/2; 
-		rightMaxBound = pos.z + bounds.z/2; 
-		upMaxBound = pos.x + bounds.x/2; 
-		downMaxBound = pos.x - bounds.x/2; 
-
-		// print ("leftMaxBound is" + leftMaxBound);
-		// print ("rightMaxBound is" + rightMaxBound);
-		// print ("upMaxBound is" + upMaxBound);
-		// print ("downMaxBound is" + downMaxBound);
-
-		pos.z += Random.Range (1.0f,zSize);
-//		pos.y += ySize;
-
-		this.transform.position = pos;
-
-//		LayEggs();
-//		InvokeRepeating("LayEggs", 3.0f, 4.5f);
+		//set threshold distance or position
+		float thresholdDistance = xSize;
+		thresholdDistance = thresholdDistance/2; 
+		thresholdMax = platform.transform.position.x + thresholdDistance - 2.0f; 
+		thresholdMin = platform.transform.position.x - thresholdDistance + 2.0f; 
 
 		dir = 1;
-		speed = Random.Range (0.2f,0.8f);
-
-//		print ("starting position of chicken is "+ pos.y);
-
+		speed = Random.Range (0.01f,0.3f);
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = new Vector3 (1.0f,0.0f,0.0f); 
-//		transform.RotateAround(platform.transform.position,Vector3.down,0.5f);
-//		Vector3 movement= new Vector3(Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f)); 
+		print(this.transform.rotation);
+		//move the dragon
+		this.transform.localPosition += Time.deltaTime * Vector3.right * speed * dir;
 
-//		this.transform.localPosition += Time.deltaTime * movement * speed * dir;
-//		if (this.transform.position > bounds){
-//			this.transform.rotation = new Quaternion (0,1,0,0);
-//			dir = -1;
-//		} else if (this.transform.position < bounds){
-//			this.transform.rotation = new Quaternion (0,0,0,1);
-//			dir = 1;
-//		}
-//		Vector3.up; Vector3.forward;Vector3.right;
-
-
-//		Vector3 currentPosition = this.transform.position;
-//		currentPosition += new Vector3(Random.Range(-0.1f,0.1f),0.0f,Random.Range(-0.1f,0.1f));
-//
-
-		//annotated from
-		// Check Left Map Bound
-//		if (currentPosition.x < -(leftMaxBound))
-//		{
-//			currentPosition = Vector3.Lerp(
-//				this.transform.position,
-//				new Vector3(leftMaxBound, currentPosition.y, currentPosition.z),
-//				speed * Time.deltaTime);
-//		}
-//		// Check Left Map Bound
-//
-//
-//		// Check Down Map Bound
-//		if (currentPosition.z < -(downMaxBound))
-//		{
-//			currentPosition = Vector3.Lerp(
-//				this.transform.position,
-//				new Vector3(this.transform.position.x, currentPosition.y, downMaxBound),
-//				speed * Time.deltaTime);
-//		}
-//		// Check Down Map Bound
-//
-//
-//		// Check Right Map Bound
-//		if (currentPosition.x > rightMaxBound)
-//		{
-//			currentPosition = Vector3.Lerp(
-//				currentPosition,
-//				new Vector3(rightMaxBound, currentPosition.y, currentPosition.z),
-//				speed * Time.deltaTime);
-//		}
-//		// Check Right Map Bound
-//
-//
-//		// Check Up Map Bound
-//		if (currentPosition.z > upMaxBound )
-//		{
-//			currentPosition = Vector3.Lerp(
-//				currentPosition,
-//				new Vector3(currentPosition.x, currentPosition.y, upMaxBound),
-//				speed * Time.deltaTime);
-//		}
-//		// Check Up Map Bound
-//
-//		this.transform.position = currentPosition;
+		//change the direction it is moving to
+		//make object look towards the direction it is moving
+		if (this.transform.position.x > thresholdMax){
+			this.transform.rotation = new Quaternion (0,-1,0,1);
+			dir = -1;
+		} else if (this.transform.position.x < thresholdMin){
+			this.transform.rotation = new Quaternion (0,1,0,1);
+			dir = 1;
+		}
 	}
 
-//	void LayEggs()
-//	{
-//		GameObject newEgg = Instantiate<GameObject>(egg);
-//
-//		Vector3 position = this.gameObject.transform.position;
-//		position.y += ySize; 
-//		newEgg.transform.position = position;
-//	}
-
-	public void AssignPlatform(GameObject newPlatform){
-		this.platform = newPlatform; 
-	}
 
 
 
